5179fe21edd4e6a721bfd892eea12eaee55dc663
Surekha <surekhagidithuri@gmail.com>
2025-07-18 21:09:32 +0530

    roboshop components

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
   for_each      = var.components
-  source        = "../../terraform-aws-roboshop"
+  source        = "git::https://github.com/SurekhaBandaru/terraform-aws-roboshop.git?ref=main"
   component     = each.key #cart, shipping, payment
   rule_priority = each.value.rule_priority
 
